d9d759a572acd281c0c5b8127d93751a323dced3
Thong Dang <thongdn.it@gmail.com>
2018-11-09 13:29:48 +0700

    add admob into project

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,13 +1,13 @@
 import { createSwitchNavigator } from 'react-navigation'
 
-import MainTabNavigator from './MainTabNavigator'
 import AuthLoadingScreen from '../screens/AuthLoadingScreen'
 import SignInScreen from '../screens/SignInScreen'
+import MainScreen from '../screens/MainScreen'
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   AuthLoading: AuthLoadingScreen,
-  Main: MainTabNavigator,
+  Main: MainScreen,
   Auth: SignInScreen,
 })
